3ee68f0bdcf05cb20ee0b6f684499be5bf28201b
jhonlopez <jhon.lopez@cyxtera.com>
2021-03-11 14:03:49 -0500

    Project with bad password handle.

---
+++ src/main/java/com/example/testbadpasswordhandle/TestBadPasswordHandleApplication.java
@@ -0,0 +1,13 @@
+package com.example.testbadpasswordhandle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBadPasswordHandleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBadPasswordHandleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testbadpasswordhandle/TestBadPasswordHandleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testbadpasswordhandle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBadPasswordHandleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
